4b158df1658caa2cd4b510f5680d81b9d955c61d
Sunny <sunny@Sunnys-MacBook-Pro.local>
2025-03-23 19:51:58 +1100

    fixed animation

--- src/components/CardAnimation.tsx
+++ src/components/CardAnimation.tsx
@@ -4,16 +4,16 @@ const CardAnimation: React.FC = () => {
   return (
     <>
       {/* Top-Left Corner */}
-      <AnimationBox boxLocation="-top-2 -left-2 border-t-6 border-l-6" />
+      <AnimationBox boxLocation="-top-2 -left-2 border-t-6 border-l-6 origin-top-left" />
 
       {/* Top-Right Corner */}
-      <AnimationBox boxLocation="-top-2 -right-2 border-t-6 border-r-6" />
+      <AnimationBox boxLocation="-top-2 -right-2 border-t-6 border-r-6 origin-top-right" />
 
       {/* Bottom-Left Corner */}
-      <AnimationBox boxLocation="-bottom-2 -left-2 border-b-6 border-l-6" />
+      <AnimationBox boxLocation="-bottom-2 -left-2 border-b-6 border-l-6 origin-bottom-left" />
 
       {/* Bottom-Right Corner */}
-      <AnimationBox boxLocation="-right-2 -bottom-2 border-r-6 border-b-6" />
+      <AnimationBox boxLocation="-right-2 -bottom-2 border-r-6 border-b-6 origin-bottom-right" />
     </>
   );
 };
